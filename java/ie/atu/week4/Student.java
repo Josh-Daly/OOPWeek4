befb075aaf1ff54c081d14a10ce36bda95a57adc
Josh-Daly <g00470372@atu.ie>
2025-10-06 09:53:41 +0100

    Adding Student class

---
+++ java/ie/atu/week4/Student.java
@@ -0,0 +1,15 @@
+package ie.atu.week4;
+
+public class Student
+{
+    //private String studentName;
+    //private String studentEmail;
+    //private String studentCourse;
+
+    //public Student(String name, String email, String course)
+    //{
+        //this.studentName = name;
+        //this.studentEmail = email;
+        //this.studentCourse = course;
+    //}
+}
