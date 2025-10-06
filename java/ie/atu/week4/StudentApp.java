0016e2ef3cf38ebee03e1062323a38411fc9abc0
Josh-Daly <g00470372@atu.ie>
2025-10-06 09:28:49 +0100

    Initial Commit

---
+++ java/ie/atu/week4/StudentApp.java
@@ -0,0 +1,18 @@
+// Paul Daly
+// OOP Week 4
+// Student class to store student details using arrayList
+package ie.atu.week4;
+
+
+
+public class StudentApp {
+    public static void main(String[] args) {
+
+
+
+
+
+    }
+}
+
+
